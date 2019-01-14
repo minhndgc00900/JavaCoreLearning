2f80b7b25e94068d13e8bfccf1842bb8bc6dfc6b
minhndgc00900 <minhnd0496@gmail.com>
2019-01-14 21:19:14 +0700

    second commitment

---
+++ src/charAndBoolean.java
@@ -0,0 +1,17 @@
+public class charAndBoolean
+{
+    public static void main(String[] args)
+    {
+        //with char, can only put one type of character
+        //for example, it is incorrect if the var has
+        //the value like this 't%'. However, it is allowed
+        //to use unicode
+        char myChar = '\u00A9'; //one example of using unicode, the result is ©
+        System.out.println(myChar);
+
+        //boolean can only true or false;
+        boolean myB = false;
+
+
+    }
+}
---
+++ src/string.java
@@ -0,0 +1,20 @@
+public class string
+{
+    public static void main(String[] args)
+    {
+        String b = "2";
+        double c = 3.33d;
+        b += c; //b = b + c
+        String str1 = "minh";
+        String age  = "23";
+        String sentence = "my name is " + str1 + " Im " + age;
+        String copyright = "\u00A9 2019";
+        System.out.println(sentence);
+        System.out.println(copyright);
+
+        //we cannot calculate two values if
+        // they are or one of them is string
+        System.out.println(b + age);
+        System.out.println(b);
+    }
+}
